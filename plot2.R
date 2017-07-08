132f86b5a72431af3bf784d7142318fd791ec8ce
aclyrics65M <fgmamagh315@gmail.com>
2017-07-08 18:22:26 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,8 @@
+# R Function for Plot 2 (Line Plot)
+plot2 <- function() {
+        plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+        dev.copy(png, file="plot2.png", width=480, height=480)
+        dev.off()
+        cat("plot2.png has been saved in", getwd())
+}
+plot2()
