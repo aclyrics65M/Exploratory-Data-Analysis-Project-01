46a07cec3b7c8f8158ea17a207df4869862ea3ed
aclyrics65M <fgmamagh315@gmail.com>
2017-07-08 18:23:59 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-# R Funtion for Plot 1 (Histogram Plot)
+# R Function for Plot 1 (Histogram Plot)
 plot1 <- function() {
         hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
         dev.copy(png, file="plot1.png", width=480, height=480)
