9a5b1bdf589a16bde35a5331e3de9517f52edc4e
aclyrics65M <fgmamagh315@gmail.com>
2017-07-08 20:46:56 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,26 +1,31 @@
 # R Function for Plot 4 (Combined Charts/Graphs)
+
+# Function plot4 displays four charts/graphs in one display.
 plot4 <- function() {
         par(mfrow=c(2,2))
         
         ##PLOT 1
-        plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+        plot(df$timestamp,df$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+        
         ##PLOT 2
-        plot(df$timestamp,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+        plot(df$timestamp,df$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
         
         ##PLOT 3
-        plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-        lines(df$timestamp,df$Sub_metering_2,col="red")
-        lines(df$timestamp,df$Sub_metering_3,col="blue")
+        plot(df$timestamp,df$Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering")
+        lines(df$timestamp,df$Sub_metering_2,col = "red")
+        lines(df$timestamp,df$Sub_metering_3,col = "blue")
         legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) 
         
         #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
         
         #PLOT 4
-        plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+        plot(df$timestamp,df$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
         
         #OUTPUT
-        dev.copy(png, file="plot4.png", width=480, height=480)
+        dev.copy(png, file="plot4.png", width = 480, height = 480)
         dev.off()
         cat("plot4.png has been saved in", getwd())
 }
+
+# Command for displaying plot4
 plot4()
