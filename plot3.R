e5c0b25c6fd0ac7cf598b4e48b845b9b0e38831b
aclyrics65M <fgmamagh315@gmail.com>
2017-07-08 20:30:09 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,6 +1,8 @@
 # R Function of Plot 3 (Energy Sub Metering Plot)
+
+# Function plot3 creates a graph known as Energy Sub Metering Plot that plots 3 distinct lines with different colors of black, red, blue. 
 plot3 <- function() {
-        plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+        plot(df$timestamp,df$Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering")
         lines(df$timestamp,df$Sub_metering_2,col="red")
         lines(df$timestamp,df$Sub_metering_3,col="blue")
         legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
@@ -8,4 +10,6 @@ plot3 <- function() {
         dev.off()
         cat("plot3.png has been saved in", getwd())
 }
+
+# Command to display plot3
 plot3()
